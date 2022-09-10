4d093cb732a771a690f33d2b4155f7f416e87814
Ruan Scherer <ruan.scherer@paytrack.com.br>
2022-09-10 16:31:41 -0300

    feat: home page banner

---
+++ components/Home/Banner/index.tsx
@@ -0,0 +1,51 @@
+import { Box, Heading, Image, Text } from "@chakra-ui/react";
+
+export function Banner() {
+  return (
+    <Box
+      bgImage="url('/home-banner-background.svg')"
+      bgRepeat="no-repeat"
+      bgPosition="center"
+      bgSize="cover"
+      width="full"
+      py={[4, 8, 20]}
+      px={[12, 20, 36]}
+    >
+      <Box
+        maxWidth="900px"
+        position="relative"
+        mx="auto"
+      >
+        <Box maxWidth={["none", "none", "none", "50%"]}>
+          <Heading
+            fontSize={[20, 36]}
+            fontWeight="medium"
+            color="light.500"
+          >
+            5 Continentes,
+            <br />
+            infinitas possibilidades
+          </Heading>
+          <Text
+            mt={5}
+            fontSize={[14, 20]}
+            color="light.200"
+          >
+            Chegou a hora de tirar do papel a viagem que você sempre sonhou.
+          </Text>
+        </Box>
+        
+        <Image
+          src="/airplane.svg"
+          position="absolute"
+          transform="rotate(3deg)"
+          right="-1rem"
+          bottom={["-3rem", "-5rem", "-7rem"]}
+          width="24rem"
+          display={["none", "none", "none", "block"]}
+          alt="Avião amarelo com detalhe branco e nuvens brancas ao redor"
+        />
+      </Box>
+    </Box>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import type { NextPage } from 'next'
 import { Header } from "../components/Header"
+import { Banner } from "../components/Home/Banner"
 
 const Home: NextPage = () => {
   return (
-    <Header />
+    <>
+      <Header />
+      <Banner />
+    </>
   )
 }
 
